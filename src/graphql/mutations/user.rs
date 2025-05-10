f4f83d753ace19c88af434d5e9e573b0b0fdf04d
Benedikt Nerb <nerb.benedikt@gmail.com>
2025-05-10 19:21:13 +0200

    Add loginUser mutation
    This commit also changes the user table migration to use an argon2id hash of a simple password.

--- src/graphql/mutations/user.rs
+++ src/graphql/mutations/user.rs
@@ -1,10 +1,13 @@
+use std::sync::Arc;
+
 use argon2::{
     password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
-    Argon2,
+    Argon2, PasswordHash, PasswordVerifier,
 };
 use async_graphql::{Context, InputObject, Result};
 use diesel::prelude::*;
 
+use crate::auth::{create_jwt, Claims, JwtKeyPair};
 use crate::db::{conn::DbPool, models::user::DbUser};
 use crate::graphql::queries::GqlUser;
 use crate::schema::users;
@@ -15,17 +18,53 @@ pub struct CreateUserInput {
     pub password: String,
 }
 
+#[derive(Debug, InputObject)]
+pub struct LoginUserInput {
+    pub email: String,
+    pub password: String,
+}
+
 #[derive(Default)]
 pub struct UserMutations;
 
 #[async_graphql::Object]
 impl UserMutations {
+    async fn login_user(&self, ctx: &Context<'_>, input: LoginUserInput) -> Result<String> {
+        // Get DB connection
+        let pool = ctx.data::<DbPool>()?;
+        let conn = &mut pool.get().unwrap();
+
+        // Check if there is a user with the given email
+        let user = users::table
+            .filter(users::email.eq(input.email))
+            .first::<DbUser>(conn)
+            .map_err(|_| async_graphql::Error::new("Invalid email or password"))?;
+
+        // Check given password against hash from DB
+        // TODO: Think about if we want to support old bcrypt hashes
+        let parsed_hash = PasswordHash::new(&user.password_hash)?;
+        let password_ok = Argon2::default()
+            .verify_password(input.password.as_bytes(), &parsed_hash)
+            .is_ok();
+        if !password_ok {
+            return Err(async_graphql::Error::new("Invalid email or password"));
+        }
+
+        // Extract (reference to) encoding key from context
+        let encoding_key = &ctx.data::<Arc<JwtKeyPair>>()?.encoding_key;
+        // Create JWT for this user
+        // TODO: Error handling
+        let jwt = create_jwt(&Claims::new(user.email), encoding_key).unwrap();
+
+        Ok(jwt)
+    }
+
     async fn create_user(&self, ctx: &Context<'_>, input: CreateUserInput) -> Result<GqlUser> {
         // Get DB connection
         let pool = ctx.data::<DbPool>()?;
         let conn = &mut pool.get().unwrap();
 
-        // Hash password
+        // Generate random salt for password hash
         let salt = SaltString::generate(&mut OsRng);
 
         // Argon2 with default params (Argon2id v19)
