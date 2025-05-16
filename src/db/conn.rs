d20a8f1b0b6fb13bb5487f58c46392f08990e2fe
Benedikt Nerb <nerb.benedikt@gmail.com>
2025-05-16 23:17:22 +0200

    Add error handling and logging to main functions

--- src/db/conn.rs
+++ src/db/conn.rs
@@ -1,14 +1,19 @@
+use anyhow::Context;
 use diesel::prelude::*;
-use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+use log::info;
 
 use crate::config::DatabaseConfig;
 
 // Type alias to simplify code
 pub type DbPool = Pool<ConnectionManager<PgConnection>>;
+pub type DbConn = PooledConnection<ConnectionManager<PgConnection>>;
 
-pub fn create_db_pool(db_config: &DatabaseConfig) -> DbPool {
+pub fn create_db_pool(db_config: &DatabaseConfig) -> anyhow::Result<DbPool> {
     let manager = ConnectionManager::<PgConnection>::new(db_config.url());
-    Pool::builder()
+    let pool = Pool::builder()
         .build(manager)
-        .expect("Unable to create pool")
+        .context("Unable to establish (enough) connection(s) to database")?;
+    info!("Successfully connected to database");
+    Ok(pool)
 }
