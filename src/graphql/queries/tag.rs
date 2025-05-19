d467ec442b408ddee55758f3b869becb37b6473e
Benedikt Nerb <nerb.benedikt@gmail.com>
2025-05-19 11:17:06 +0200

    Add error handling and logging to tag queries and mutations

--- src/graphql/queries/tag.rs
+++ src/graphql/queries/tag.rs
@@ -1,10 +1,9 @@
 use async_graphql::{Context, Result, SimpleObject};
 use diesel::prelude::*;
 
-use crate::db::{
-    conn::DbPool,
-    models::tag::{DbTag, TagPriority},
-};
+use crate::db::models::tag::{DbTag, TagPriority};
+use crate::graphql::error::GqlApiError;
+use crate::graphql::util::get_conn_from_ctx;
 use crate::schema::tags::dsl::*;
 
 #[derive(Debug, SimpleObject)]
@@ -39,13 +38,15 @@ impl TagQueries {
     async fn tags(&self, ctx: &Context<'_>) -> Result<Vec<GqlTag>> {
         // NOTE: Using the TagLoader is not beneficial here since (for now) we don't filter on ids
 
-        // Get DB conn
-        let pool = ctx.data::<DbPool>()?;
-        let conn = &mut pool.get().unwrap();
+        // Get DB connection
+        let conn = &mut get_conn_from_ctx(ctx)?;
 
         // Construct and execute query
-        let query = tags.select(DbTag::as_select());
-        let results: Vec<DbTag> = query.load(conn).expect("Error loading tags");
+        let results = tags
+            .select(DbTag::as_select())
+            .load(conn)
+            .map_err(|e| GqlApiError::internal("Error while loading tags", e.to_string()))?;
+
         Ok(results.into_iter().map(Into::into).collect())
     }
 }
