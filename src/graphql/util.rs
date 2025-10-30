2c8e08ba346b27d4e075b0c61a096f587028fd60
Benedikt Nerb <nerb.benedikt@gmail.com>
2025-10-30 19:50:22 +0100

    Apply clippy suggestions

--- src/graphql/util.rs
+++ src/graphql/util.rs
@@ -42,7 +42,7 @@ pub fn get_conn_from_ctx(ctx: &async_graphql::Context) -> async_graphql::Result<
         .data::<DbPool>()
         .map_err(|e| GqlApiError::internal("Unable to get pool from context.", e.message))?;
 
-    Ok(get_conn_from_pool(pool)?)
+    get_conn_from_pool(pool)
 }
 
 pub fn get_conn_from_pool(pool: &DbPool) -> async_graphql::Result<DbConn> {
