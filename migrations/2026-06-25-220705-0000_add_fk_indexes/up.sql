287eeb00a76f8ef26129a5b9a1c0a1a6037296ae
Benedikt Nerb <nerb.benedikt@gmail.com>
2026-06-27 13:39:15 +0200

    Reorder indices in migration

--- migrations/2026-06-25-220705-0000_add_fk_indexes/up.sql
+++ migrations/2026-06-25-220705-0000_add_fk_indexes/up.sql
@@ -1,12 +1,10 @@
--- Your SQL goes here
+-- Plain FK indexes
+CREATE INDEX idx_images_review ON images (review);
+CREATE INDEX idx_dishes_aliases_dish ON dishes_aliases (dish);
+CREATE INDEX idx_occurrences_dish ON occurrences (dish);
 
 -- occurrences() often filters by location and date
 CREATE INDEX idx_occurrences_location_date ON occurrences (location, date);
-
--- plain FK indexes
-CREATE INDEX idx_occurrences_dish ON occurrences (dish);
-CREATE INDEX idx_images_review ON images (review);
-CREATE INDEX idx_dishes_aliases_dish ON dishes_aliases (dish);
 
 -- ReviewLoader / review images join filter by occurrence
 -- INCLUDE (stars) lets review_data.metadata's avg(stars)/count(id) run as an index-only scan
